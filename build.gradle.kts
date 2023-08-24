34bec2fd13eb54c0eb0f54de77d2f17860f9a7bd
Sam Gammon <sam@elide.ventures>
2023-08-24 16:47:58 -0700

    chore: version bump → `0.9.10`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
 import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
-    embeddedKotlin("multiplatform")
+    kotlin("multiplatform") version "1.9.10"
     alias(libs.plugins.testlogger)
     alias(libs.plugins.versionCheck)
     alias(libs.plugins.doctor)
