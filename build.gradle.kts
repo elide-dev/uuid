3f6a510cafd5a79dfeca95c14feb1406e5bf7d9f
Sam Gammon <sam@elide.ventures>
2024-05-06 22:26:25 -0700

    chore: upgrade kotlin → `2.0.0-RC2`
    - chore: upgrade kotlin
    - chore: upgrade all applicable deps
    - chore: regen lock files
    - chore: update verification metadata
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,24 +20,24 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
-  kotlin("multiplatform") version "2.0.0-Beta3" apply false
-  kotlin("plugin.serialization") version "2.0.0-Beta3" apply false
-  alias(libs.plugins.testlogger) apply false
-  alias(libs.plugins.kotlinx.benchmark) apply false
+    kotlin("multiplatform") version "2.0.0-RC2" apply false
+    kotlin("plugin.serialization") version "2.0.0-RC2" apply false
+    alias(libs.plugins.testlogger) apply false
+    alias(libs.plugins.kotlinx.benchmark) apply false
 
-  alias(libs.plugins.dokka)
-  alias(libs.plugins.versionCheck)
-  alias(libs.plugins.sonar)
-  alias(libs.plugins.kover)
-  alias(libs.plugins.detekt)
-  alias(libs.plugins.nexus)
-  alias(libs.plugins.spdx.sbom)
-  alias(libs.plugins.cyclonedx)
-  alias(libs.plugins.sigstore)
-  alias(libs.plugins.kotlinx.apiValidator)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.versionCheck)
+    alias(libs.plugins.sonar)
+    alias(libs.plugins.kover)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.nexus)
+    alias(libs.plugins.spdx.sbom)
+    alias(libs.plugins.cyclonedx)
+    alias(libs.plugins.sigstore)
+    alias(libs.plugins.kotlinx.apiValidator)
 
-  `project-report`
-  signing
+    `project-report`
+    signing
 }
 
 val enableSbom = true
@@ -53,138 +53,138 @@ group = GROUP
 version = VERSION
 
 detekt {
-  parallel = true
-  ignoreFailures = true
-  config.setFrom(rootProject.files(".github/detekt.yml"))
+    parallel = true
+    ignoreFailures = true
+    config.setFrom(rootProject.files(".github/detekt.yml"))
 }
 
 sonar {
-  properties {
-    listOf(
-      "sonar.projectKey" to "elide-dev_uuid",
-      "sonar.organization" to "elide-dev",
-      "sonar.host.url" to "https://sonarcloud.io",
-      "sonar.coverage.jacoco.xmlReportPaths" to "${project.rootDir}/build/reports/kover/report.xml",
-    ).forEach { (key, value) ->
-      property(key, value)
+    properties {
+        listOf(
+            "sonar.projectKey" to "elide-dev_uuid",
+            "sonar.organization" to "elide-dev",
+            "sonar.host.url" to "https://sonarcloud.io",
+            "sonar.coverage.jacoco.xmlReportPaths" to "${project.rootDir}/build/reports/kover/report.xml",
+        ).forEach { (key, value) ->
+            property(key, value)
+        }
     }
-  }
 }
 
 nexusPublishing {
-  repositories {
-    sonatype {
-      nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
-      snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    repositories {
+        sonatype {
+            nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
+            snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }
     }
-  }
 }
 
 tasks.cyclonedxBom {
-  setIncludeConfigs(listOf("jvmRuntimeClasspath"))
-  setProjectType("library")
-  setDestination(project.file("build/reports"))
-  setOutputFormat("all")
-  setIncludeBomSerialNumber(true)
-  setComponentVersion("2.0.0")
+    setIncludeConfigs(listOf("jvmRuntimeClasspath"))
+    setProjectType("library")
+    setDestination(project.file("build/reports"))
+    setOutputFormat("all")
+    setIncludeBomSerialNumber(true)
+    setComponentVersion("2.0.0")
 }
 
 tasks.build {
-  if (isReleaseBuild) {
-    finalizedBy(":subprojects:uuid-core:spdxSbomForRelease")
-  }
+    if (isReleaseBuild) {
+        finalizedBy(":subprojects:uuid-core:spdxSbomForRelease")
+    }
 }
 
 subprojects {
-  apply(plugin = "dev.sigstore.sign")
-  apply(plugin = "org.jetbrains.dokka")
-  apply(plugin = "org.jetbrains.kotlinx.kover")
-  apply(plugin = "org.sonarqube")
-  apply(plugin = "signing")
+    apply(plugin = "dev.sigstore.sign")
+    apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+    apply(plugin = "org.sonarqube")
+    apply(plugin = "signing")
 }
 
 val reports: TaskProvider<Task> by tasks.registering {
-  group = "reports"
+    group = "reports"
 
-  dependsOn(
-    tasks.koverXmlReport,
-    tasks.dependencyReport,
-    tasks.htmlDependencyReport,
-  )
+    dependsOn(
+        tasks.koverXmlReport,
+        tasks.dependencyReport,
+        tasks.htmlDependencyReport,
+    )
 }
 
 val preMerge: TaskProvider<Task> by tasks.registering {
-  group = "test"
-  description = "Run all tests and checks"
+    group = "test"
+    description = "Run all tests and checks"
 
-  listOfNotNull(
-    tasks.build,
-    tasks.check,
-    tasks.koverXmlReport,
-    if (enableSbom) tasks.spdxSbom else null,
-    if (enableCyclonedx) tasks.cyclonedxBom else null,
-    reports,
-  ).forEach {
-    dependsOn(it)
-  }
-  if (sonarScan == "true") {
-    dependsOn(
-      tasks.sonar,
-    )
-  }
+    listOfNotNull(
+        tasks.build,
+        tasks.check,
+        tasks.koverXmlReport,
+        if (enableSbom) tasks.spdxSbom else null,
+        if (enableCyclonedx) tasks.cyclonedxBom else null,
+        reports,
+    ).forEach {
+        dependsOn(it)
+    }
+    if (sonarScan == "true") {
+        dependsOn(
+            tasks.sonar,
+        )
+    }
 }
 
 rootProject.plugins.withType(NodeJsRootPlugin::class.java) {
-  rootProject.the<NodeJsRootExtension>().download = true
-  rootProject.the<NodeJsRootExtension>().version = nodeVersion
-  if (nodeVersion.contains("canary")) {
-    rootProject.the<NodeJsRootExtension>().downloadBaseUrl = "https://nodejs.org/download/v8-canary"
-  }
+    rootProject.the<NodeJsRootExtension>().download = true
+    rootProject.the<NodeJsRootExtension>().version = nodeVersion
+    if (nodeVersion.contains("canary")) {
+        rootProject.the<NodeJsRootExtension>().downloadBaseUrl = "https://nodejs.org/download/v8-canary"
+    }
 }
 rootProject.plugins.withType(YarnPlugin::class.java) {
-  rootProject.the<YarnRootExtension>().yarnLockMismatchReport = YarnLockMismatchReport.WARNING
-  rootProject.the<YarnRootExtension>().reportNewYarnLock = false
-  rootProject.the<YarnRootExtension>().yarnLockAutoReplace = true
+    rootProject.the<YarnRootExtension>().yarnLockMismatchReport = YarnLockMismatchReport.WARNING
+    rootProject.the<YarnRootExtension>().reportNewYarnLock = false
+    rootProject.the<YarnRootExtension>().yarnLockAutoReplace = true
 }
 tasks.withType(org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask::class.java).configureEach {
-  args.add("--ignore-engines")
+    args.add("--ignore-engines")
 }
 
 plugins.withType(io.gitlab.arturbosch.detekt.DetektPlugin::class) {
-  tasks.withType(io.gitlab.arturbosch.detekt.Detekt::class) detekt@{
-    reports.sarif.required = true
-    reports.sarif.outputLocation = rootProject.layout.buildDirectory.file("reports/detekt/report.sarif")
-  }
+    tasks.withType(io.gitlab.arturbosch.detekt.Detekt::class) detekt@{
+        reports.sarif.required = true
+        reports.sarif.outputLocation = rootProject.layout.buildDirectory.file("reports/detekt/report.sarif")
+    }
 }
 
 if (lockDeps == "true") {
-  dependencyLocking {
-    lockAllConfigurations()
-    lockMode = LockMode.LENIENT
-  }
+    dependencyLocking {
+        lockAllConfigurations()
+        lockMode = LockMode.LENIENT
+    }
 }
 
 val resolveAndLockAll: TaskProvider<Task> by tasks.registering {
-  group = "build"
-  description = "Resolve and re-lock all dependencies"
+    group = "build"
+    description = "Resolve and re-lock all dependencies"
 
-  doFirst {
-    require(gradle.startParameter.isWriteDependencyLocks)
-  }
-  doLast {
-    configurations.filter {
-      // Add any custom filtering on the configurations to be resolved
-      it.isCanBeResolved
-    }.forEach { it.resolve() }
-  }
+    doFirst {
+        require(gradle.startParameter.isWriteDependencyLocks)
+    }
+    doLast {
+        configurations.filter {
+            // Add any custom filtering on the configurations to be resolved
+            it.isCanBeResolved
+        }.forEach { it.resolve() }
+    }
 }
 
 val relock: TaskProvider<Task> by tasks.registering {
-  group = "build"
-  description = "Re-lock all dependencies"
+    group = "build"
+    description = "Re-lock all dependencies"
 
-  dependsOn(
-    tasks.dependencies,
-    resolveAndLockAll,
-  )
+    dependsOn(
+        tasks.dependencies,
+        resolveAndLockAll,
+    )
 }
